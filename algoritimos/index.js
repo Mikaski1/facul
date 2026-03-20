f08f885fe9ddf9c0f1a2e473c79bb6a611bb66de
Mikaski1 <anthonyyamorim@gmail.com>
2026-03-19 21:48:14 -0300

    Add files via upload
    sla, atividade repetitiva de estados

---
+++ algoritimos/index.js
@@ -0,0 +1,144 @@
+/*let n1 = 7
+let n2 = 8
+let n3 = 8
+let media = (n1 + n2 + n3) / 3
+console.log(`A média é ${media.toFixed(1)}`)
+
+if(media >=7){
+console.log("Aprovado")
+}
+*/ 
+
+
+// TEMPERATURA
+
+/*let temperatura = 101
+
+if(temperatura > 100){ 
+ console.log('morreu')
+}
+
+else if(temperatura >= 35){ 
+ console.log('muito quente')
+}
+
+else if(temperatura < -50){
+    console.log('morreu')
+}
+
+else if(temperatura > 20){
+    console.log('quente')
+}
+
+else{
+    console.log('frio')
+} */
+
+// VIDA
+/*
+let vida = 55
+
+if(vida > 70){
+    console.log('saudável')
+}
+
+else if (vida > 40){
+    console.log('cuidado')
+}
+
+else{
+    console.log('perigo')
+} */
+
+    // DIRIGIR
+/*
+    let idade = 18
+
+    let temCarteira = false
+
+    if (idade >= 18){
+        console.log('você já tem idade para fazer sua carteira')
+    }
+
+    if (temCarteira){
+        console.log('Você já tem a sua carteira')
+    }
+
+    else{
+        console.log('Você não tem idade para fazer sua carteira')
+    }
+        */
+
+    // PAR OU IMPAR
+    /*
+    let n = 8
+
+    if(n % 2 === 0){ 
+        console.log('É par')
+    }
+    else {
+        console.log('É impar')
+    }
+        */
+
+    // LOGIN              && negação desse ||
+/*
+    let usuario = "admin"
+    let senha = "1234"
+
+    if(usuario === "admin" && senha === "1234"){ 
+        console.log('Login correto')
+    }
+    else{
+        console.log('Erro no login')
+    }
+        */
+
+    //PLACAR DE JOGO
+    /*
+let time1;
+let time2;
+
+time1 = window.prompt("Escreva o placar do time 1");
+time2 = window.prompt("Escreva o placar do time 2");
+time1 = Number(time1);
+time2 = Number(time2)
+
+
+    if(time1 > time2){
+        console.log('Time 1 venceu!')
+    }
+    else if(time2 > time1){
+        console.log('Time 2 venceu!')
+    }
+    else{
+        console.log('Empate!')
+    }
+*/
+
+// CALCULADORA
+/*
+let n1 = 2
+let n2 = 2
+let op = "+"
+
+if(op === "+"){
+    console.log(n1 + n2)
+}
+
+else if(op === "-"){
+    console.log(n1 - n2)
+}
+    
+else if(op === "*"){
+    console.log(n1 * n2)
+}
+
+else if(op === "/"){
+    console.log(n1 / n2)
+}
+
+else{
+    console.log('Operação inválida')
+}
+    */
