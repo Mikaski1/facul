2447d1ae86200c153cf9e807bb9193d44e32d18c
Mikaski1 <anthonyyamorim@gmail.com>
2026-04-23 20:32:34 -0300

    Add files via upload

---
+++ condição while/revisao/001/index.js
@@ -0,0 +1,70 @@
+/*1- ?
+
+=== - igual a x
+
+&& - e
+
+! - para inverter a lógica,
+
+2-estruturas condicionais
+
+if, if else, if else if else
+
+let
+
+switch -
+
+
+
+
+
+
+
+
+
+
+cd "nome da pasta"
+npm install prompt-sync
+node index.js
+*/
+/*
+const prompt
+const n1 = Number(prompt("digite N1: "))
+const n2 = Number(prompt("digite N2: "))
+
+const media = (n1+n2) / 2
+
+if (media >= 7){
+    console.log("aprovado")
+}
+else {
+    console.log("reprovado")
+}*/
+//triangulos
+/*
+const prompt = require("prompt-sync")()
+
+const n1 = Number(prompt("digite N1: "))
+
+const n2 = Number(prompt("digite N2: "))
+
+const n3 = Number(prompt("digite N3: "))
+
+if ( n1 > n2 + n3 || n2 > n3 + n1 || n3 > n1 + n2){
+    console.log("Não forma um traingulo")
+}
+
+else if (  n1 !== n2 && n2 !== n3 && n1 !== n3 ){
+console.log("Escaleno")
+}
+
+else if ( n1 === n2 && n2 === n3 && n1 === n3){
+    console.log("Equilátero")
+}
+
+
+else {
+    console.log("Isóceles")
+
+}
+*/
